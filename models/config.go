0ba41a7c8066c2e13a7bc12b2abd4c6565ffc9d1
Kevin Ho <kevin.ho01@sap.com>
2026-09-01 14:09:11 -0700

    move handling of fileFlag to config struct

--- models/config.go
+++ models/config.go
@@ -3,6 +3,8 @@ package models
 import (
 	"errors"
 	"fmt"
+	"manifest-seperator/filereader"
+	"os"
 
 	"github.com/goccy/go-yaml"
 )
@@ -19,6 +21,9 @@ func (c *Config) CombineFiles() error {
 	}
 	if len(c.FileMap) == 0 {
 		return errors.New("FileMap is empty")
+	}
+	if len(c.Data) > 0 {
+		return errors.New("Cannot CombineFiles, config.Data is non-empty")
 	}
 	var res []byte
 	var err error
@@ -84,4 +89,35 @@ func (c *Config) SetMode(modeFlag *string) error {
 		return errors.New("unknown mode flag. Must be one of the following: dash, list, appset")
 	}
 	return nil
+}
+
+func (c *Config) HandleFileFlag(fileFlag *string) error {
+
+	var err error
+	if *fileFlag == "" {
+		c.Data, err = filereader.ReadStdIn()
+		if err != nil {
+			return err
+		}
+		return nil
+
+	}
+
+	fileInfo, err := os.Stat(*fileFlag)
+	if err != nil {
+		return err
+	}
+	switch {
+	case fileInfo.Mode().IsDir():
+		fmt.Printf("Reading dir: %v\n", *fileFlag)
+		c.FileMap, err = filereader.ReadDir(*fileFlag)
+	case fileInfo.Mode().IsRegular():
+		fmt.Printf("Reading file: %v\n", *fileFlag)
+		c.Data, err = os.ReadFile(*fileFlag)
+	}
+	if err != nil {
+		return err
+	}
+	return nil
+
 }
